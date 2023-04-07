55e5ac72796923da0b7a37ca19d1f0fee6eaaf2e
Jhudharh <judahitah@gmail.com>
2023-04-07 03:10:35 -0700

    body_size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# Send a GET request to the URL and save the response to a variable
-response=$(curl -s $1)
-
-# Get the length of the response body in bytes
+#To GET the byte of the HTTP response header for a given URL
 curl -s $1 | wc -c
