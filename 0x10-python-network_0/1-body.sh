86730644b27ac7cbbf623ff5212fccc13815b256
Jhudharh <judahitah@gmail.com>
2023-04-07 03:15:48 -0700

    body_sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -4,5 +4,5 @@ response=$(curl -s -I -w "%{http_code}" $1)
 
 # Check if the response status code is 200
 if [[ $response == *" 200"* ]]; then
-    # Display the body of the response
-    curl -s $1
+# Display the body of the response
+curl -s $1
