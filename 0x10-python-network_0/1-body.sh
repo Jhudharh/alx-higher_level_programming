c5445365cf760002a0c546f4801d2a2311a44e36
Jhudharh <judahitah@gmail.com>
2023-04-07 03:17:39 -0700

    body fixed

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-# Send a GET request to the URL and save the response to a variable
-response=$(curl -s -I -w "%{http_code}" $1)
-
 # Check if the response status code is 200
-if [[ $response == *" 200"* ]]; then
 # Display the body of the response
 curl -s $1
