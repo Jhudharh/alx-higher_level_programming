037b86a830dd353a56a872a2bd04a8e42d4e40e3
Jhudharh <judahitah@gmail.com>
2023-04-26 03:12:33 +0000

    updated

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
-const fs = require('fs');
-const file_path = process.argv[2];
-fs.readFile(file_path, 'utf8', (err, data) => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log(data);
-  }
-});
 
+const fs = require('fs');
+
+fs.readFile(process.argv[2], 'utf8', (error, content) => {
+  console.log(error || content);
+});
