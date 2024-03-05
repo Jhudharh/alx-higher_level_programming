f2330859eb5cb9058abdddfa77cdfb730e68e083
Itah Judah Nduka <judahitah@gmail.com>
2024-03-05 06:26:48 +0100

    Delete 0x15-javascript-web_jquery directory

--- 0x15-javascript-web_jquery/4-script.js
+++
@@ -1,8 +0,0 @@
-$('DIV#toggle_header').click(function () {
-  const header = $('HEADER');
-  if (header.hasClass('red')) {
-    header.removeClass('red').addClass('green');
-  } else {
-    header.removeClass('green').addClass('red');
-  }
-});
--- 0x15-javascript-web_jquery/8-script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-  $.get('https://swapi-api.alx-tools.com/api/films/?format=json', function (data) {
-    $.each(data.results, function (i, movie) {
-      $('#list_movies').append('<li>' + movie.title + '</li>');
-    });
-  });
-});
